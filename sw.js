7917ef08d6b9fd8a89e7e6290ca13878d145657d
bellehou2024 <bellehou2024@gmail.com>
2026-04-30 21:06:32 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "fresh-weight-assistant-v14";
+const ASSET_PATHS = [
+  "./",
+  "./index.html",
+  "./src/main.js",
+  "./src/styles.css",
+  "./src/pwa.js",
+  "./src/domain/health.js",
+  "./src/domain/id.js",
+  "./src/storage/localStore.js",
+  "./src/sync/syncService.js",
+  "./public/manifest.webmanifest",
+  "./public/icon.svg"
+];
+
+function assetUrl(path) {
+  return new URL(path, self.registration.scope).toString();
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSET_PATHS.map(assetUrl))));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => {
+        if (event.request.mode === "navigate") return caches.match(assetUrl("./index.html"));
+        return caches.match(event.request);
+      })
+  );
+});
