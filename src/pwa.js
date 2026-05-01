c69b6edb40c0742a1581f81339dfd53a7daae352
bellehou2024 <bellehou2024@gmail.com>
2026-05-01 10:52:58 +0800

    Update pwa.js

--- src/pwa.js
+++ src/pwa.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js", { scope: "./" }).catch(() => {
+    navigator.serviceWorker.register("/sw.js", { scope: "/" }).catch(() => {
       // The app still works if service worker registration is unavailable.
     });
   });
